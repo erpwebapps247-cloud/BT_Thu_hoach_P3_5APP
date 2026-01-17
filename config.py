71eb78786f332f28face4bb1dc81e7d76922aa21
unknown <ERPwebapps247@gmail.com>
2026-01-17 14:56:41 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# File cấu hình - KHÔNG commit file này vào git
+# Thêm config.py vào .gitignore
+
+# OpenAI API Key
+OPENAI_API_KEY = "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"
